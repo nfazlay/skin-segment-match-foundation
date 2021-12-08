5b8a37ddbde47fa00e8ce0fcd569a418c3ba09c6
Fazlay Rabbi <fazlayrabbi@cmail.carleton.ca>
2021-12-08 17:59:15 -0500

    FEAT: New features added

--- detectskin.py
+++ detectskin.py
@@ -4,49 +4,43 @@ import cv2
 import imutils as im
 import argparse
 
-# parse command line arguments if any
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-v", "--video", help = "path to video (optional)")
-# args = vars(ap.parse_args())
+class DetectSkin:
+    def __init__(self):
+        """Constructor for the DetectSkin Class
+        """
+        self.min_HSV = np.array([0, 48, 80], dtype = "uint8")
+        self.max_HSV = np.array([20, 255, 255], dtype = "uint8")
 
-# setting the upper and lower boundaries of the HSV pixels
-# min_HSV = np.array([0, 133, 77], np.uint8)
-# max_HSV = np.array([235, 173, 127], np.uint8)
-min_HSV = np.array([0, 48, 80], dtype = "uint8")
-max_HSV = np.array([20, 255, 255], dtype = "uint8")
+    def run(self):
+        # start the webcam
+        camera = cv2.VideoCapture(0)
 
-# start the webcam
-camera = cv2.VideoCapture(0)
-
-if not camera.isOpened():
-    raise IOError("cannot open webcam")
+        if not camera.isOpened():
+            raise IOError("cannot open webcam")
 
 
-while True:
-    ret, frame = camera.read()
-    ret, frame = camera.read()
-    dim = (400, 200)
-    frame = im.resize(frame, width= 400)
-    #resized = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
-    # frame = cv2.resize(frame, None, fx = 0.5, fy = 0.5, interpolation=cv2.INTER_AREA)
-    #convert_to_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2YCR_CB)
-    convert_to_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    dmask = cv2.inRange(convert_to_HSV, min_HSV, max_HSV)
-    
-    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
-    # dmask = cv2.erode(dmask, kernel, iterations = 2)
-    # dmask = cv2.dilate(dmask, kernel, iterations = 2)
-    dmask = cv2.GaussianBlur(dmask, (3,3), 0)
+        while True:
+            ret, frame = camera.read()
 
-    skin = cv2.bitwise_and(frame, frame, mask= dmask)
+            frame = im.resize(frame, width= 400)
+            #convert_to_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2YCR_CB)
+            convert_to_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            dmask = cv2.inRange(convert_to_HSV, self.min_HSV, self.max_HSV)
+            
+            dmask = cv2.GaussianBlur(dmask, (3,3), 0)
 
-    cv2.imshow('images', np.hstack([frame, skin]))
+            skin = cv2.bitwise_and(frame, frame, mask= dmask)
 
-    cv2.imwrite("./test.png", np.hstack([frame, skin]))
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            cv2.imshow('images', np.hstack([frame, skin]))
 
-camera.release()
-cv2.destroyAllWindows()
+            cv2.imwrite("./test.png", skin)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        camera.release()
+        cv2.destroyAllWindows()
+
+d = DetectSkin()
+d.run()
 
 
